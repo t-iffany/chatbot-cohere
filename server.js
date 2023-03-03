e273071f65aec1de16f8647865c64961dd11cfd8
t-iffany <tiffanyxleong@gmail.com>
2023-03-02 23:02:04 -0800

    Create server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+// basic express server
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// listen for incoming connections and set up a socket.io connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  // listen for disconnect events
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// set up server to listen on port 3000
+http.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
